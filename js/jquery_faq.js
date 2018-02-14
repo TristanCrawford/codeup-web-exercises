463cc69df244e1798dbe67e0cc5d8b7dea1fcc9b
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-02-14 12:04:19 -0600

    Finished FAQ Bonuses

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,11 +1,11 @@
 "use strict";
 {
 
-    let dt = $('dt');
+    $('h2,h3,li').css('cursor','pointer');
 
     // Show <dd> when <dt> is clicked.
     $('dd').addClass('invisible');
-    dt.click(function (ev) {
+    $('dt').click(function (ev) {
         $(this).next().toggleClass('invisible');
     });
 
@@ -27,12 +27,6 @@
         $(this).parent().children().first().css('color', 'blue');
     });
 
-    // Hide newsletter nag if <span> is clicked.
-    $('#newsletter span').click(function (ev) {
-        $(this).parent().slideToggle();
-    });
-
-    // After 8 seconds, fade in the hidden <h2>.
-    $('h2').hide();
-    setTimeout(function() { $('h2').fadeToggle(); }, 8000);
+    // After 8 seconds, fade in the modal.
+    setTimeout(function() { $("#myModal").modal('show'); }, 8000);
 }
